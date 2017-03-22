7b28eb76d933112abb59fac47f5a0f8a7d33f7ed
Wilker Alves Nogueira <wilkeralves97@gmail.com>
2017-03-22 11:52:18 -0300

    curso completo js

---
+++ js/busca-pacientes.js
@@ -0,0 +1,25 @@
+var buscaPacientes = document.querySelector("#buscar-pacientes");
+buscaPacientes.addEventListener("click",function(){
+  console.log("vou buscar os pacientes...");
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET","https://api-pacientes.herokuapp.com/pacientes");
+
+  xhr.addEventListener("load",function(){
+    var erroAjax = document.querySelector("#erro-ajax");
+
+    if(xhr.status == 200){
+      erroAjax.classList.add("invisivel");
+      var resposta = xhr.responseText;
+      var pacientes = JSON.parse(resposta);
+      pacientes.forEach(function(paciente){
+        adicionaPacientesNaTabela(paciente);
+      })
+    }else {
+      erroAjax.classList.remove("invisivel");
+      console.log(xrh.status);
+    }
+
+  });
+
+  xhr.send();
+});
